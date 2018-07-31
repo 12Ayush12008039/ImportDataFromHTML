9f7d399b7341390addf36d43976f065443312d8f
Melih Pekmez <melihw2@gmail.com>
2018-07-31 12:00:13 +0300

    Create product.cs

---
+++ product.cs
@@ -0,0 +1,15 @@
+class Product
+    {
+        public string title;
+        public string category;
+        public string price;
+        public string quantity;
+        
+        public Product(string Titles,string Categories,string Prices,string Quantities)
+        {
+            title = Titles;
+            category = Categories;
+            price = Prices;
+            quantity = Quantities;
+        }
+    }
